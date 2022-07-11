c5d12acced3205e46abfa9379b46ed7abe6c00b4
hueifeng <695979933@qq.com>
2022-07-11 23:04:16 +0800

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using OrleansURLShortener;
+
+Dictionary<string,long> list = new Dictionary<string, long>();
+
+for (long i = 0; i < 10000000000; i++)
+{
+    var hash = ShortUrlGenerator.MurmurHash3(i + "test");
+    var code = ShortUrlGenerator.Generator(hash);
+    //var code = GetCode(hash);
+    list.Add(code, i);
+}
+
+//Parallel.For(0, 10000000000, (i) => 
+//{
+//    var hash = ShortUrlGenerator.MurmurHash3(i + "test");
+//    var code = ShortUrlGenerator.Generator(hash);
+//    //var code = GetCode(hash);
+//    list.Add(code,i);
+//});
+
+
+//var hash = ShortUrlGenerator.MurmurHash3("111121233121");
+//var code1 = GetCode(hash);
+//var code = ShortUrlGenerator.Generator(hash);
+
+//var hash = ShortUrlGenerator.MurmurHash3("test");
+var isRepeat = list.GroupBy(i => i).Where(g => g.Count() > 1);
+var count = isRepeat.Count();
+Console.ReadLine();
--- OrleansURLShortener/Program.cs
+++ OrleansURLShortener/Program.cs
@@ -1,14 +1,44 @@
+using System.Net;
+using System.Reflection;
+using System.Text;
 using Microsoft.AspNetCore.Http.Extensions;
 using Orleans;
+using Orleans.Configuration;
 using Orleans.Hosting;
 using Orleans.Runtime;
+using OrleansURLShortener;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Host.UseOrleans(siloBuilder =>
 {
-    siloBuilder.UseLocalhostClustering();
-    siloBuilder.AddMemoryGrainStorage("urls");
+    //siloBuilder.UseLocalhostClustering();
+    //siloBuilder.AddMemoryGrainStorage("urls");
+    siloBuilder
+        .UseLocalhostClustering()
+        //.UseAdoNetClustering(options =>
+        //{
+        //    options.Invariant = "Npgsql";
+        //    options.ConnectionString =
+        //        "Server=localhost;Port=5432;User Id=postgres;Password=mysecretpassword;Database=test.db";
+        //})
+        .Configure<ClusterOptions>(options =>
+        {
+            options.ClusterId = "dev";
+            options.ServiceId = "OrleansBasics";
+        })
+        .Configure<EndpointOptions>(options =>
+        {
+            options.AdvertisedIPAddress = IPAddress.Loopback;
+        })
+        .ConfigureApplicationParts(x => x.AddApplicationPart(Assembly.GetEntryAssembly()))
+        .AddAdoNetGrainStorage("urls", options =>
+        {
+            options.Invariant = "Npgsql";
+            options.ConnectionString =
+                "Server=localhost;Port=5432;User Id=postgres;Password=mysecretpassword;Database=test.db";
+        });
+
 });
 
 var app = builder.Build();
@@ -19,7 +49,8 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/shorten/{*path}", async (HttpContext context, string path) =>
 {
-    var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+    // var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+    var shortenedRouteSegment = ShortUrlGenerator.MurmurHash3(path).ToString();
     var shortenerGrain = grainFactory.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
     await shortenerGrain.SetUrl(shortenedRouteSegment, path);
     var resultBuilder = new UriBuilder(context.Request.GetEncodedUrl())
@@ -40,29 +71,45 @@ app.MapGet("/go/{shortenedRouteSegment}", async (string shortenedRouteSegment) =
 
 app.Run();
 
+public class UrlDictionary
+{
+    public UrlDictionary() { }
+    public UrlDictionary(string code, string hashval)
+    {
+        Code = code;
+        HashVal = hashval;
+    }
+    public string Code { get; set; }
+
+    public string HashVal { get; set; }
+
+    public string Url { get; set; }
+
+    public DateTime InsertAt { get; set; }
+}
 
 public class UrlShortenerGrain : Grain, IUrlShortenerGrain
 {
-    private IPersistentState<KeyValuePair<string, string>> _cache;
+    private IPersistentState<UrlDictionary> _cache;
 
     public UrlShortenerGrain(
         [PersistentState(
             stateName: "url",
             storageName: "urls")]
-            IPersistentState<KeyValuePair<string, string>> state)
+            IPersistentState<UrlDictionary> state)
     {
         _cache = state;
     }
 
     public async Task SetUrl(string shortenedRouteSegment, string fullUrl)
     {
-        _cache.State = new KeyValuePair<string, string>(shortenedRouteSegment, fullUrl);
+        _cache.State = new UrlDictionary(shortenedRouteSegment, fullUrl);
         await _cache.WriteStateAsync();
     }
 
     public Task<string> GetUrl()
     {
-        return Task.FromResult(_cache.State.Value);
+        return Task.FromResult(_cache.State.HashVal);
     }
 }
 
